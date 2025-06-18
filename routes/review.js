036d7f2927631b7e03d522920b8f6b72bd7983a5
ripuranjan-143 <ripuranjan143@gmail.com>
2025-06-18 14:35:21 +0530

    Err wrapasync2

--- routes/review.js
+++ routes/review.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
-const wrapAsync = require("../utils/WrapAsync")
+const wrapAsync = require("../utils/WrapAsync.js")
 const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 const reviewController = require("../controllers/reviews.js")
 
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const wrapAsync = require("../utils/WrapAsync")
+const wrapAsync = require("../utils/WrapAsync.js")
 const passport = require("passport");
 const {saveRedirectUrl} = require("../middleware.js");
 const userController = require("../controllers/users.js")
